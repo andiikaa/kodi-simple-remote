836cca0204c8079d2f122fd6618cd8d7e2d13a86
andre <andre.kuehnie@gmail.com>
2016-03-19 11:26:12 +0100

    share project

---
+++ src/main/java/org/github/kodisimpleremote/App.java
@@ -0,0 +1,13 @@
+package org.github.kodisimpleremote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
